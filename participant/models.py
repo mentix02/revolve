be07a2030f58875b8ed51971e49d4b0eebb8253a
mentix02 <manan.yadav02@gmail.com>
2020-09-25 06:36:52 +0530

    Changed reverse related_names for Participant model.

--- participant/models.py
+++ participant/models.py
@@ -4,8 +4,12 @@ from django.db import models
 class Participant(models.Model):
 
     timestamp = models.DateTimeField(auto_now_add=True)
-    user = models.ForeignKey('user.User', on_delete=models.CASCADE)
-    protest = models.ForeignKey('protests.Protest', on_delete=models.CASCADE)
+    user = models.ForeignKey(
+        'user.User', on_delete=models.CASCADE, related_name='participating_in'
+    )
+    protest = models.ForeignKey(
+        'protests.Protest', on_delete=models.CASCADE, related_name='participants'
+    )
 
     class Meta:
         unique_together = ['user', 'protest']
