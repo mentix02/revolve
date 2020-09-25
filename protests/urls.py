0b34d79aebfcbc7071e78a7e231390e6ea1290fe
mentix02 <manan.yadav02@gmail.com>
2020-09-25 13:15:09 +0530

    Added urls for delete and update views.

--- protests/urls.py
+++ protests/urls.py
@@ -1,11 +1,21 @@
 from django.urls import path
 
-from protests.views import index, ProtestCreateView, ProtestDetailView
+from protests.views import (
+    index,
+    ProtestEditView,
+    ProtestDetailView,
+    ProtestCreateView,
+    ProtestDeleteView,
+    DashboardListView,
+)
 
 app_name = 'protests'
 
 urlpatterns = [
     path('', index, name='index'),
     path('create/', ProtestCreateView.as_view(), name='create'),
+    path('dashboard/', DashboardListView.as_view(), name='dashboard'),
+    path('edit/<slug:slug>/', ProtestEditView.as_view(), name='edit'),
+    path('delete/<slug:slug>/', ProtestDeleteView.as_view(), name='delete'),
     path('detail/<slug:slug>/', ProtestDetailView.as_view(), name='detail'),
 ]
